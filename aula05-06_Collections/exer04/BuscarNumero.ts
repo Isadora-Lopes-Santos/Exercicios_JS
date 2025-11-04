bb244d066011de74fc841c872c3de5fdc5106ef8
Isadora <isadoraloopes@hotmail.com>
2025-11-03 23:51:36 -0300

    Aula05-06

---
+++ aula05-06_Collections/exer04/BuscarNumero.ts
@@ -0,0 +1,30 @@
+import leia = require ("readline-sync")
+
+const numeros: Set<number> = new Set<number>()
+
+console.log("Digite 10 números inteiros (não repetidos):", "\n")
+
+while (numeros.size < 10) {
+  const num = leia.questionInt(`Numero ${numeros.size + 1}: `)
+
+  if (numeros.has(num)) {
+    console.log("Valor repetido! Digite outro número.")
+  } else {
+    numeros.add(num)
+  }
+}
+console.clear()
+
+console.log("\nList:")
+console.log([...numeros].join(","))
+
+const busca = leia.questionInt("\nDigite o numero que voce deseja encontrar: ")
+
+console.clear()
+
+console.log()
+if (numeros.has(busca)) {
+  console.log(`O numero ${busca} foi encontrado!`)
+} else {
+  console.log(`O numero ${busca} não foi encontrado!`)
+}
